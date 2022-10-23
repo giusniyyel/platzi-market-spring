f586e19d526de9dbd241738c9e97528d9f40fd5d
Daniel Campos <37324092+giusniyyel@users.noreply.github.com>
2022-10-23 13:21:23 -0500

    :wrench: chore: update methods to return domain objects instead of entities

--- src/main/java/com/giusniyyel/platzimarket/persistence/ProductRepository.java
+++ src/main/java/com/giusniyyel/platzimarket/persistence/ProductRepository.java
@@ -1,36 +1,50 @@
 package com.giusniyyel.platzimarket.persistence;
 
+import com.giusniyyel.platzimarket.domain.ProductDTO;
+import com.giusniyyel.platzimarket.domain.repository.ProductRepositoryDTO;
 import com.giusniyyel.platzimarket.persistence.crud.ProductCrudRepository;
 import com.giusniyyel.platzimarket.persistence.entity.Product;
+import com.giusniyyel.platzimarket.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements ProductRepositoryDTO {
     private ProductCrudRepository productCrudRepository;
+    private ProductMapper mapper;
 
-    public List<Product> getAll() {
-        return (List<Product>) productCrudRepository.findAll();
+    @Override
+    public List<ProductDTO> getAll() {
+        List<Product> products = (List<Product>) productCrudRepository.findAll();
+        return mapper.toProductsDTO(products);
     }
 
-    public List<Product> getByCategory(int idCategory) {
-        return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+    @Override
+    public Optional<List<ProductDTO>> getByCategory(int idCategory) {
+        List<Product> products = productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+        return Optional.of(mapper.toProductsDTO(products));
     }
 
-    public Optional<List<Product>> getOutOfStock(int stock) {
-        return productCrudRepository.findByStockLessThanAndStatus(stock, true);
+    @Override
+    public Optional<List<ProductDTO>> getScarseProducts(int quantity) {
+        Optional<List<Product>> products = productCrudRepository.findByStockLessThanAndStatus(quantity, true);
+        return products.map(prods -> mapper.toProductsDTO(prods));
     }
 
-    public Optional<Product> getProduct(int idProduct) {
-        return productCrudRepository.findById(idProduct);
+    @Override
+    public Optional<ProductDTO> getProduct(int idProduct) {
+        return productCrudRepository.findById(idProduct).map(prod -> mapper.toProductDTO(prod));
     }
 
-    public Product save(Product product) {
-        return productCrudRepository.save(product);
+    @Override
+    public ProductDTO save(ProductDTO product) {
+        Product prod = mapper.toProduct(product);
+        return mapper.toProductDTO(productCrudRepository.save(prod));
     }
 
+    @Override
     public void delete(int idProduct) {
         productCrudRepository.deleteById(idProduct);
     }
