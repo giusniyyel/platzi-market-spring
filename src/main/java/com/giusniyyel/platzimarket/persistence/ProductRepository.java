b8de4f3debf198a797106dc8b633d418b9445afd
Daniel Campos <37324092+giusniyyel@users.noreply.github.com>
2022-10-23 11:56:21 -0500

    :wrench: chore: add repository annotation

--- src/main/java/com/giusniyyel/platzimarket/persistence/ProductRepository.java
+++ src/main/java/com/giusniyyel/platzimarket/persistence/ProductRepository.java
@@ -2,10 +2,12 @@ package com.giusniyyel.platzimarket.persistence;
 
 import com.giusniyyel.platzimarket.persistence.crud.ProductCrudRepository;
 import com.giusniyyel.platzimarket.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductRepository {
 
     public Optional<List<Product>> getOutOfStock(int stock) {
         return productCrudRepository.findByStockLessThanAndStatus(stock, true);
+    }
+
+    public Optional<Product> getProduct(int idProduct) {
+        return productCrudRepository.findById(idProduct);
+    }
+
+    public Product save(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(int idProduct) {
+        productCrudRepository.deleteById(idProduct);
     }
 }
