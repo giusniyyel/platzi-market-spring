ecb386751b8ed43a317db93d6eac7541f6108cfc
Daniel Campos <37324092+giusniyyel@users.noreply.github.com>
2022-10-23 12:53:04 -0500

    :wrench: chore: add mappers for domain objects & entities

--- src/main/java/com/giusniyyel/platzimarket/persistence/mapper/CategoryMapper.java
+++ src/main/java/com/giusniyyel/platzimarket/persistence/mapper/CategoryMapper.java
@@ -1,2 +1,22 @@
-package com.giusniyyel.platzimarket.persistence.mapper;public interface CategoryMapper {
+package com.giusniyyel.platzimarket.persistence.mapper;
+
+import com.giusniyyel.platzimarket.domain.CategoryDTO;
+import com.giusniyyel.platzimarket.persistence.entity.Category;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategory", target = "categoryId"),
+            @Mapping(source = "description", target = "category"),
+            @Mapping(source = "status", target = "active"),
+    })
+    CategoryDTO toCategoryDTO(Category category);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "products", ignore = true)
+    Category toCategory(CategoryDTO categoryDTO);
 }
--- src/main/java/com/giusniyyel/platzimarket/persistence/mapper/ProductMapper.java
+++ src/main/java/com/giusniyyel/platzimarket/persistence/mapper/ProductMapper.java
@@ -1,2 +1,27 @@
-package com.giusniyyel.platzimarket.persistence.mapper;public interface ProductMapper {
+package com.giusniyyel.platzimarket.persistence.mapper;
+
+import com.giusniyyel.platzimarket.domain.ProductDTO;
+import com.giusniyyel.platzimarket.persistence.entity.Product;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "idProduct", target = "productId"),
+            @Mapping(source = "idCategory", target = "categoryId"),
+            @Mapping(source = "salePrice", target = "price"),
+            @Mapping(source = "status", target = "active"),
+    })
+    ProductDTO toProductDTO(Product product);
+
+    List<ProductDTO> toProductsDTO(List<Product> products);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codeBar", ignore = true)
+    Product toProduct(ProductDTO productDTO);
 }
