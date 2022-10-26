4b465cdbd86842799df5ee2b23ee0fdbfb41436d
Daniel Campos <37324092+giusniyyel@users.noreply.github.com>
2022-10-26 11:05:24 -0500

    :wrench: chore: add purchase mappings

--- src/main/java/com/giusniyyel/platzimarket/persistence/mapper/PurchaseItemMapper.java
+++ src/main/java/com/giusniyyel/platzimarket/persistence/mapper/PurchaseItemMapper.java
@@ -1,2 +1,27 @@
-package com.giusniyyel.platzimarket.persistence.mapper;public interface PurchaseItemMapper {
+package com.giusniyyel.platzimarket.persistence.mapper;
+
+import com.giusniyyel.platzimarket.domain.PurchaseItemDTO;
+import com.giusniyyel.platzimarket.persistence.entity.SaleProduct;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class}) // Product Mapper is required just to ignore it.
+public interface PurchaseItemMapper {
+
+    @Mappings({
+            @Mapping(source = "id.idProduct", target = "productId"),
+            @Mapping(source = "quantity", target = "quantity"),
+            @Mapping(source = "status", target = "active")
+    })
+    PurchaseItemDTO toPurchaseItem(SaleProduct product);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "sale", ignore = true),
+            @Mapping(target = "product", ignore = true),
+            @Mapping(target = "id.idSale", ignore = true)
+    })
+    SaleProduct toSaleProduct(PurchaseItemDTO item);
 }
--- src/main/java/com/giusniyyel/platzimarket/persistence/mapper/PurchaseMapper.java
+++ src/main/java/com/giusniyyel/platzimarket/persistence/mapper/PurchaseMapper.java
@@ -1,2 +1,27 @@
-package com.giusniyyel.platzimarket.persistence.mapper;public interface PurchaseMapper {
+package com.giusniyyel.platzimarket.persistence.mapper;
+
+import com.giusniyyel.platzimarket.domain.PurchaseDTO;
+import com.giusniyyel.platzimarket.persistence.entity.Sale;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {PurchaseItemMapper.class})
+public interface PurchaseMapper {
+
+    @Mappings({
+            @Mapping(source = "idSale", target = "purchaseId"),
+            @Mapping(source = "idClient", target = "clientId"),
+            @Mapping(source = "paymentType", target = "paymentMethod"),
+            @Mapping(source = "products", target = "items"),
+    })
+    PurchaseDTO toPurchase(Sale sale);
+    List<PurchaseDTO> toPurchases(List<Sale> sales);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "cliente", ignore = true)
+    Sale toSale(PurchaseDTO purchaseDTO);
 }
